68f47111a33f0fe5813d36168684d08e33684050
louis-reinaldo <117139220+louis-reinaldo@users.noreply.github.com>
2023-05-28 16:04:57 +0800

    modified train, hpo and register model for homework2

--- cohorts/2023/02-experiment-tracking/homework/register_model.py
+++ cohorts/2023/02-experiment-tracking/homework/register_model.py
@@ -11,8 +11,9 @@ from sklearn.metrics import mean_squared_error
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
 EXPERIMENT_NAME = "random-forest-best-models"
 RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state', 'n_jobs']
+MODEL_REGISTRY_NAME = "random-forest-reg"
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
@@ -71,9 +72,16 @@ def run_register_model(data_path: str, top_n: int):
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    model_uri = f"runs:/{best_run.info.run_id}/model"
+    mlflow.register_model(model_uri, MODEL_REGISTRY_NAME)
 
 
 if __name__ == '__main__':
--- cohorts/2023/02-experiment-tracking/homework/train.py
+++ cohorts/2023/02-experiment-tracking/homework/train.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("mlops_zoomcamp_homework2")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,14 +22,20 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
+        
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        tree_max_depth = 10
+        mlflow.log_param("max_depth", tree_max_depth)
+        
+        rf = RandomForestRegressor(max_depth=tree_max_depth, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
